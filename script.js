138483a318f0624b40ea75cfd26e14c0bbaf6713
ShehzorMemon <Shezymemon0180@gmail.com>
2023-03-19 15:33:19 +0500

    Added all the files.

---
+++ script.js
@@ -0,0 +1,26 @@
+const header = document.getElementById('header')
+const title = document.getElementById('title')
+const excerpt = document.getElementById('excerpt')
+const profile_img = document.getElementById('profile_img')
+const name = document.getElementById('name')
+const date = document.getElementById('date')
+
+const animated_bgs = document.querySelectorAll('.animated-bg')
+const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
+
+setTimeout(getData, 2500)
+
+function getData() {
+    header.innerHTML = '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQi0jEyMDd9&auto=format&fit=crop&w=crop&w=2102&q=80" alt="">'
+    title.innerHTML = 'Testimonial of recent client'
+    excerpt.innerHTML = 'I highly recommend him to anyone who needs a professional, user-friendly website for their business or personal use.'
+    profile_img.innerHTML = '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="">'
+    name.innerHTML = 'Timba Bavuma'
+    date.innerHTML = 'March 19, 2023'
+
+    animated_bgs.forEach(bg => bg.classList.remove('animated-bg-text'))
+    animated_bg_texts.forEach(bg => bg.classList.remove('animated-bg-text'))
+
+}
+
+
